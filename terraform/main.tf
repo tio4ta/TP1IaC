d3ef58d0cbb9416ccac3e9623b3d5891e4544553
crystal <crystalboudaud@gmail.com>
2026-01-09 10:31:44 +0100

    Étape 5 : Création d'un conteneur Docker avec Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,48 @@ terraform {
  
 # Provider local → Terraform agit sur ta machine locale 
 provider "local" {} 
+
+############################################################ 
+# Configuration principale Terraform 
+############################################################ 
+terraform { 
+  # On indique ici quels "providers" Terraform doit utiliser 
+  required_providers { 
+    docker = { 
+      source  = "kreuzwerker/docker" # Provider Docker officiel 
+      version = "~> 3.0"             # Version minimum du plugin Docker 
+    } 
+  } 
  
-# Ressource locale : création d'un fichier 
+}
+
+############################################################ 
+# Provider Docker 
+############################################################ 
+# Ce bloc indique que Terraform va utiliser Docker 
+# comme environnement cible pour créer des ressources. 
+provider "docker" { 
+  # Pas besoin de configuration supplémentaire 
+  # Docker est déjà installé en local 
+} 
+ 
+############################################################ 
+# Ressource : Conteneur Docker NGINX 
+############################################################ 
+resource "docker_container" "nginx" { 
+  image = "nginx:latest"        # Image Docker utilisée (téléchargée si absente) 
+  name  = "terraform-nginx"     # Nom du conteneur 
+ 
+  # Configuration des ports 
+  # Le port 80 interne du conteneur devient accessible 
+  # via le port 8080 sur la machine locale 
+  ports { 
+    internal = 80               # Port dans le conteneur 
+    external = 8080             # Port sur la machine hôte 
+  } 
+}
+
+### Ressource locale : création d'un fichier 
 resource "local_file" "hello" { 
   filename = var.file_name     # On remplace la valeur par une variable 
   content  = var.file_content  # Idem pour le contenu 
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,15 @@
 ############################## 
-# Outputs visibles après apply 
+# Outputs - Docker container 
 ############################## 
  
-output "file_path" { 
- 
-  description = "Chemin complet du fichier créé" 
-  value       = local_file.hello.filename 
+# Nom du conteneur Docker créé 
+output "container_name" { 
+  description = "Nom du conteneur Docker créé par Terraform" 
+  value       = docker_container.nginx.name 
 } 
  
-output "file_content" { 
-  description = "Contenu écrit dans le fichier" 
-  value       = local_file.hello.content 
+# Port d'accès externe 
+output "published_port" { 
+  description = "Port externe pour accéder à NGINX" 
+  value       = docker_container.nginx.ports[0].external 
 }
